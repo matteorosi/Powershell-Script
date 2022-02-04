9a12c7a4c38bb33c4bf38e90942d04f6f326e6f8
matteorosi <53863633+matteorosi@users.noreply.github.com>
2022-02-04 11:33:52 +0100

    Update Deploy_Update.ps1

--- Deploy_Update.ps1
+++ Deploy_Update.ps1
@@ -1,30 +1,36 @@
-$Folder = 'C:\_\Windows_Update\'
-if (Test-Path  $Folder) {
-    "Path exists!"
 Remove-Item "C:\_\Windows_Update\" -Recurse -Force -Confirm:$false 
-} else {
-    "Path doesn't exist"
-}
-Set-ExecutionPolicy Unrestricted -force
 $winVer = [System.Environment]::OSVersion.Version.Major
-$dir = 'C:\_\Windows_Update\packages'
-mkdir $dir
+$kbinstall = get-hotfix | where {$_.HotFixID -Match "KB4023057"}
+mkdir C:\_\Windows_Update\packages
+Powercfg /x -standby-timeout-ac 0
 
+if ($kbinstall.HotFixID  -eq "KB4023057")
+ { 
 if ($winVer -eq 10)
-    {  
+     { 
         $webClient = New-Object System.Net.WebClient
         $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
-        $file = "$($dir)\Win10Upgrade.exe"
+        $file = "C:\_\Windows_Update\packages\Win10Upgrade.exe"
         $webClient.DownloadFile($url,$file)
-        Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+        Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /norestart /qn /copylogs C:\_\Windows_Update\packages'
         } 
     
     else 
 
         {
-            echo "This is Not Windows10 OS Compatible"
-        }
-    
+            echo "this version is not compatible"
+        }}
+    else
+	 { 
+			echo "KB4023057 Not Present"
+			$a=get-location
+			$b=$a.Path+"\PSWindowsUpdate"
+			New-Item -Path "C:\Windows\System32\WindowsPowerShell\v1.0\Modules" -Name "PSWindowsUpdate" -ItemType "directory"
+			Expand-Archive "PSWindowsUpdate.zip"
+			robocopy $b C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate /s /R:5
+			Import-Module -Name C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate\PSWindowsUpdate\2.2.0.2\PSWindowsUpdate.psd1
+			Get-WindowsUpdate -Install -KBArticleID KB4023057 -AcceptAll -IgnoreReboot
+	 }
 sleep 10
 
 Remove-Item "C:\_\Windows_Update\" -Recurse -Force -Confirm:$false 
